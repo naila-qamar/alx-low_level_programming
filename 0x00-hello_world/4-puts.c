5da297fef6e1048f482d618c6042b131af311905
Naila Qamar <naailarehman11@gmail.com>
2023-10-05 07:42:03 -0700

    Add the scripts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string followed by a new line to stdout.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
