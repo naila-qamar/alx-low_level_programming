c476538894c03e21e616ad935219829c6b94f6d4
naila-qamar <144435727+naila-qamar@users.noreply.github.com>
2023-10-11 22:29:29 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,42 +1,14 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    char str[] = "_putchar\n";
-    int i;
-
-    for (i = 0; str[i] != '\0'; i++)
-        write(1, &str[i], 1);
-
-    return (0);
-}
-<<<<<<< HEAD
-#include "main.h"
-
-/**
- * main - Entry point of the program
+ * Description: Print "_putchar" followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return (0);
+printf("_putchar\n");
+return (0);
 }
-
-=======
->>>>>>> 5a94951bcd39ef2a3dca2ceaf2961e7d8b6f982e
