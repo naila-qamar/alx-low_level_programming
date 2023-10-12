ee2fd3dbb092a4f33bee0e9382d3d486f22a539f
naila-qamar <144435727+naila-qamar@users.noreply.github.com>
2023-10-12 18:28:09 +0300

    Update 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line using the character \\
+ * @n: The number of \\ characters to be printed
+ */
+void print_diagonal(int n)
+{
+    int i, j;
+
+    if (n > 0)
+    {
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < i; j++)
+            {
+                _putchar(' ');
+            }
+            _putchar('\\');
+            _putchar('\n');
+        }
+    }
+    else
+    {
+        _putchar('\n');
+    }
+}
