baeb4836ad5eb61ee31f38d2081f9ee2a3427dda
naila-qamar <144435727+naila-qamar@users.noreply.github.com>
2023-10-18 19:23:55 +0300

    Update 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+/**
+ * leet - Encodes a string into 1337.
+ * @str: The input string.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *leet(char *str)
+{
+    char leet_dict[] = "aAeEoOtTlL";
+    char leet_replacement[] = "4433007711";
+
+    int i, j;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (j = 0; leet_dict[j] != '\0'; j++)
+        {
+            if (str[i] == leet_dict[j])
+            {
+                str[i] = leet_replacement[j];
+                break;
+            }
+        }
+    }
+
+    return (str);
+}
