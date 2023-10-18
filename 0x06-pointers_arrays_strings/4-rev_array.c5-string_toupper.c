eb2dd449426d807b6af3199296617a8c6b18b1bb
naila-qamar <144435727+naila-qamar@users.noreply.github.com>
2023-10-18 19:08:31 +0300

    Update 4-rev_array.c5-string_toupper.c

--- 0x06-pointers_arrays_strings/4-rev_array.c5-string_toupper.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c5-string_toupper.c
@@ -1,18 +1,18 @@
+#include "main.h"
+
 /**
  * reverse_array - Reverses the content of an array of integers.
- * @a: An array of integers.
- * @n: The number of elements in the array.
- *
- * Return: void.
+ * @a: Pointer to the array of integers.
+ * @n: Number of elements in the array.
  */
 void reverse_array(int *a, int n)
 {
-	int i, tmp;
+    int i, tmp;
 
-	for (i = 0; i < n / 2; i++)
-	{
-		tmp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = tmp;
-	}
+    for (i = 0; i < n / 2; i++)
+    {
+        tmp = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = tmp;
+    }
 }
