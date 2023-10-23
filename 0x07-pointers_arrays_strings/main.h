4899773b06735ea3e324ce010cf9454cd1a155fe
naila-qamar <144435727+naila-qamar@users.noreply.github.com>
2023-10-23 18:16:58 +0300

    Update main.h

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Writes a character to the standard output (stdout).
+ * @c: The character to be written.
+ *
+ * Return: On success 1. On error, -1 is returned.
+ */
+int _putchar(char c);
+
+/**
+ * function_name - Brief description of the function.
+ * @param1: Description of parameter 1.
+ * @param2: Description of parameter 2 (if any).
+ *
+ * Return: Description of the return value (if any).
+ */
+
+/* Add other function prototypes here */
+
+#endif /* MAIN_H */
