2edac8b9573e6e4458e4bcda47af0c0c26f553ba
naila-qamar <144435727+naila-qamar@users.noreply.github.com>
2023-10-28 14:21:06 +0300

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,17 +1,1 @@
-int _atoi(char *s)
-{
-    int sign = 1, result = 0;
-    int i = 0;
-
-    if (s[0] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-
-    for (; s[i] != '\0'; ++i)
-        result = result * 10 + s[i] - '0';
-
-    return sign * result;
-}
 
