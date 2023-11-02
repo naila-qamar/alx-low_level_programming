14dd595ed37789642221ca998bccb058553d6ea7
naila-qamar <144435727+naila-qamar@users.noreply.github.com>
2023-11-02 22:51:32 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,34 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * array_range - Creates an array of integers from min to max.
- * @min: The first value of the array.
- * @max: The last value of the array.
- *
- * Return: The pointer to the newly created array or NULL if min > max
- *         or malloc fails.
- */
+* array_range - Creates an array of integers from min to max.
+* @min: The first value of the array.
+* @max: The last value of the array.
+*
+* Return: The pointer to the newly created array or NULL if min > max
+*         or malloc fails.
+*/
 int *array_range(int min, int max)
 {
-    int *array, i, size;
-
-    /* Check if min > max */
-    if (min > max)
-        return (NULL);
-
-    /* Calculate the size of the array */
-    size = max - min + 1;
-
-    /* Allocate memory for the array */
-    array = malloc(sizeof(int) * size);
-    if (array == NULL)
-        return (NULL);
-
-    /* Fill the array with values from min to max */
-    for (i = 0; min <= max; i++, min++)
-        array[i] = min;
-
-    return (array);
+int *array, i, size;
+/* Check if min > max */
+if (min > max)
+return (NULL);
+/* Calculate the size of the array */
+size = max - min + 1;
+/* Allocate memory for the array */
+array = malloc(sizeof(int) * size);
+if (array == NULL)
+return (NULL);
+/* Fill the array with values from min to max */
+for (i = 0; min <= max; i++, min++)
+array[i] = min;
+return (array);
 }
-
